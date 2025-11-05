fe761b12537836809841f55f9eff2ba8717f7cc5
garu7c <103008292+garu7c@users.noreply.github.com>
2025-11-05 14:12:28 -0600

    apuntar al servidor

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     WEBSOCKET_URL:
 
-        'https://simple-video-app-maovares.azurewebsites.net'
+        'vc-server-cqa8byf8g8agdvce.eastus2-01.azurewebsites.net'
 };
 
 export default config;
