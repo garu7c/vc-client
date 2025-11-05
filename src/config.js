f3b61eca8bac3a82d47a93177e3cffe159582d2f
garu7c <103008292+garu7c@users.noreply.github.com>
2025-11-05 14:22:38 -0600

    Actualiza configuración en config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     WEBSOCKET_URL:
 
-        'vc-server-cqa8byf8g8agdvce.eastus2-01.azurewebsites.net'
+        'ws://vc-server-cqa8byf8g8agdvce.eastus2-01.azurewebsites.net'
 };
 
 export default config;
